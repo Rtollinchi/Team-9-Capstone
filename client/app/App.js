de9e8f90c294e0aee63c4942e587b3a47f2b2eee
elucero9 <mannylucero49@yahoo.com>
2023-04-27 17:58:12 -0400

    template set-up

--- client/app/App.js
+++ client/app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
+import Canvas from '../features/canvas/Canvas';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <AppRoutes />
+      <Canvas />
     </div>
   );
 };
---
+++ client/features/canvas/Canvas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//EDIT ME!!
+
+const Canvas = () => {
+    return (
+        <div id='container'>
+            <div className='canvas'>HELLO</div>
+        </div>
+    )
+}
+
+export default Canvas;
