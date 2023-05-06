5714786d944b4a880dd26e658b5edc4be2abe411
nick <ntreasure14@gmail.com>
2023-05-06 18:33:51 -0400

    images for navbar added and implemented

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 /**
  * COMPONENT
  */
 const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
+  const currentDate = new Date().toLocaleDateString();
 
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <span>
+        <h3>Welcome, {username}</h3>
+        <h3>{currentDate}</h3>
+        <h3>Total Tasks Completed:</h3>
+      </span>
+      <div className="scroll-box">
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+        <p>Scroll box content goes here</p>
+      </div>
     </div>
   );
 };
