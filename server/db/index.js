e20d2f9ed3ec9171f1c6bd7f835f80ef8bab2161
nick <ntreasure14@gmail.com>
2023-05-06 13:10:35 -0400

    working on backend, models complete

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,14 @@
-//this is the access point for all things database related!
+const db = require("./db");
+const User = require("./models/User");
+const Task = require("./models/Task");
+const SubTask = require("./models/SubTask");
 
-const db = require('./db')
-
-const User = require('./models/User')
-
-//associations could go here!
+Task.belongsTo(User);
+User.hasMany(Task);
+SubTask.belongsTo(Task);
+Task.hasMany(SubTask);
 
 module.exports = {
   db,
-  models: {
-    User,
-  },
-}
+  models: { User, Task, SubTask },
+};
---
+++ server/db/models/SubTask.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const SubTask = db.define("subTask", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  taskId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
+  scheduledTime: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  dueDate: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  priority: {
+    type: Sequelize.STRING,
+  },
+  isCompleted: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+  },
+});
+
+module.exports = SubTask;
