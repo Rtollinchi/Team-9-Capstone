6ca354a30ce4a98afb506fbd71a596be74611be0
Rtollinchi <rtollinchi23@gmail.com>
2023-05-08 18:04:58 -0400

    added subTaskSlice and sub task dealing with a bug

--- client/app/store.js
+++ client/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
 import taskReducer from "../features/slices/TaskSlice";
+import subTaskReducer from "../features/slices/SubTaskSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, tasks: taskReducer },
+  reducer: { auth: authReducer, tasks: taskReducer, subTasks: subTaskReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
 export * from "../features/auth/authSlice";
 export * from "../features/slices/TaskSlice";
+export * from "../features/slices/SubTaskSlice";
--- client/features/addSubTask/AddSubTask.js
+++ client/features/addSubTask/AddSubTask.js
@@ -1,13 +1,79 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { addSubTask } from "../slices/TaskSlice";
+import DatePicker from "react-datepicker";
+import { addSubTasks } from "../slices/SubTaskSlice";
+import { fetchTasks } from "../slices/TaskSlice";
+import { selectTasks } from "../slices/TaskSlice";
+
+// import { addSubTask } from "../slices/TaskSlice
 
 const AddSubTask = () => {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch("");
+  const [task, setTask] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  // using react datepicker to grab current date
+  const [dueDate, setDueDate] = useState(new Date());
+
+  useEffect(() => {
+    dispatch(fetchTasks(currentUser.Id));
+  }, []);
+
+  const tasks = useSelector(selectTasks);
+
+  const currentUser = useSelector((state) => state.auth.me);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const taskId = task.id;
+    console.log("///////line 30",taskId);
+    dispatch(addSubTasks({ title, description, priority, dueDate, taskId }));
+    setTitle("");
+    setDescription("");
+    setPriority("");
+    setDueDate("");
+  };
 
   return (
     <div>
       <h1>add subtask here</h1>
+      <form id="subtask-form" onSubmit={handleSubmit}>
+        <select onChange={(e) => setTask(e.target.value)}>
+          {tasks.map((task) => (
+            <option key={task.id} value={task.id}>
+              {task.name}{" "}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="title">Title</label>
+        <input
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <DatePicker
+          className="border rounded"
+          selected={dueDate}
+          onChange={(date) => setDueDate(date)}
+        />
+        <label htmlFor="priority">Priority:</label>
+        <select
+          name="priority"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
