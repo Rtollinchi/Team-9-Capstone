89036a552f752cda902fe0264690fd21a7bdd74b
elucero9 <mannylucero49@yahoo.com>
2023-05-10 19:47:02 -0400

    dates from dummy data shows up on the calender

--- client/features/calendar/Calendar.js
+++ client/features/calendar/Calendar.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTasks, fetchTasks } from '../slices/TaskSlice'
 
 const Calendar = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch])
+
+  const tasks = useSelector(selectTasks);
+
+  // const [title] = tasks;
+
+  // console.log(tasks)
+
+  let calendarEvents = tasks.map(task => {
+    //  console.log(`These are the calendar events ${task.title} due on ${task.dueDate}`)
+    return { title: task.title, date: task.dueDate }
+  });
+
+  // console.log(calendarEvents)
+
   return (
     <div>
       <Fullcalendar
@@ -16,6 +38,10 @@ const Calendar = () => {
           end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
         }}
         height={"90vh"}
+        events={calendarEvents}
+        selectable={true}
+        editable={true}
+        themeSystem='mint'
       />
     </div>
   );
