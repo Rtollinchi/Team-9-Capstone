9607b82ce5b38ed817c639bfa515585163728e47
soloweshop243 <122837682+soloweshop243@users.noreply.github.com>
2023-05-15 17:17:27 -0400

    adding milddleware

--- server/api/index.js
+++ server/api/index.js
@@ -1,17 +1,39 @@
+// server/api/index.js
+
+// Import the necessary modules and models
+const router = require("express").Router();
 const User = require("../db/models/User");
 
-const router = require("express").Router();
-module.exports = router;
+// Middleware to set req.user on every request
+router.use((req, res, next) => {
+  // Get the user ID from the request (assuming it's stored in req.userId)
+  const userId = req.userId;
 
-// o: some sample to get req.user set on every request (please test before
-//  adding)
+  // Fetch the user from the database
+  User.findById(userId)
+    .then((user) => {
+      if (user) {
+        // Set req.user to the fetched user object
+        req.user = user;
+      }
+      next();
+    })
+    .catch((error) => {
+      console.error("Error fetching user:", error);
+      next(error);
+    });
+});
 
+// Mount your other route handlers
 router.use("/users", require("./users"));
 router.use("/tasks", require("./tasks"));
 router.use("/subTasks", require("./subTasks"));
 
+// 404 handler
 router.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
+
+module.exports = router;
