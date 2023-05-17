27d2e25ad27842213ddc0d699f20cad2e654758d
nick <ntreasure14@gmail.com>
2023-05-17 13:22:59 -0400

    commit

--- client/features/calendar/Calendar.js
+++ client/features/calendar/Calendar.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -8,15 +7,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectTasks, fetchTasks, updateTask } from "../slices/TaskSlice";
 import Modal from "react-modal";
 
-
 const Calendar = () => {
   const dispatch = useDispatch();
-  const tasks = useSelector(selectTasks);
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
-
 
   const tasks = useSelector(selectTasks);
   const [selectedTask, setSelectedTask] = useState(null);
@@ -40,112 +36,109 @@ const Calendar = () => {
     return {
       title: task.title,
       date: task.dueDate,
-      backgroundColor: color, 
-      extendedProps: { ...task }, 
+      backgroundColor: color,
+      extendedProps: { ...task },
     };
   });
 
   const handleEventClick = (info) => {
     const { event } = info;
-    setSelectedTask(event.extendedProps); 
+    setSelectedTask(event.extendedProps);
   };
 
   const closeModal = () => {
-    setSelectedTask(null); 
+    setSelectedTask(null);
 
-  const calendarEvents = tasks.map((task) => {
-    return { id: task.id, title: task.title, date: task.dueDate };
+    const calendarEvents = tasks.map((task) => {
+      return { id: task.id, title: task.title, date: task.dueDate };
+    });
 
-  });
+    const handleEventDrop = async (eventDropInfo) => {
+      const event = eventDropInfo.event;
 
-  const handleEventDrop = async (eventDropInfo) => {
+      const { id, start } = event;
 
-    const event = eventDropInfo.event;
+      const updatedTask = {
+        id: id,
+        dueDate: start.toISOString(),
+      };
 
-    const { id, start } = event;
-
-    const updatedTask = {
-      id: id,
-      dueDate: start.toISOString(),
+      await dispatch(updateTask(updatedTask));
     };
 
-     
-     await dispatch(updateTask(updatedTask));
+    return (
+      <div className="flex flex-col items-center justify-center h-screen overflow-auto p-6 mt-5 w-3/4 max-h-81 mx-auto rounded-md shadow-darker bg-blue-900 text-white">
+        <div className="grid grid-flow-col justify-around w-full h-full">
+          <div>
+            <Fullcalendar
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              initialView={"dayGridMonth"}
+              headerToolbar={{
+                start: "today prev,next",
+                center: "title",
+                end: "dayGridMonth,timeGridWeek,timeGridDay",
+              }}
+              height={"auto"}
+              events={calendarEvents}
+              selectable={true}
+              editable={true}
+              droppable={true}
+              eventDrop={handleEventDrop}
+              slotLabelFormat={[
+                { hour: "numeric", minute: "2-digit", hour12: true },
+              ]}
+              eventClick={handleEventClick}
+            />
+          </div>
+        </div>
 
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen overflow-auto p-6 mt-5 w-3/4 max-h-81 mx-auto rounded-md shadow-darker bg-blue-900 text-white">
-      <div className="grid grid-flow-col justify-around w-full h-full">
-
-        <div>
-          <Fullcalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView={"dayGridMonth"}
-            headerToolbar={{
-              start: "today prev,next",
-              center: "title",
-              end: "dayGridMonth,timeGridWeek,timeGridDay",
+        {selectedTask && (
+          <Modal
+            isOpen={true}
+            onRequestClose={closeModal}
+            style={{
+              content: {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                background: "white",
+                padding: "2rem",
+                borderRadius: "0.5rem",
+                boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
+                width: "400px", // Adjust the width as needed
+              },
+              overlay: {
+                position: "fixed",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                zIndex: 9999, // Set a higher value than the calendar's zIndex
+              },
             }}
-            height={"auto"}
-            events={calendarEvents}
-            selectable={true}
-            editable={true}
-            droppable={true}
-            eventDrop={handleEventDrop}
-            slotLabelFormat={[
-              { hour: "numeric", minute: "2-digit", hour12: true },
-            ]}
-            eventClick={handleEventClick} 
-          />
-        </div>
-        </div>
-
-      {selectedTask && (
-        <Modal
-        isOpen={true}
-        onRequestClose={closeModal}
-        style={{
-          content: {
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            background: "white",
-            padding: "2rem",
-            borderRadius: "0.5rem",
-            boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
-            width: "400px", // Adjust the width as needed
-          },
-          overlay: {
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            zIndex: 9999, // Set a higher value than the calendar's zIndex
-          },
-        }}
-        contentLabel="Task Details"
-        overlayClassName="modal-overlay" // Add a custom class name for the overlay
-      >
-        <h2 className="text-lg font-bold mb-4">{selectedTask.title}</h2>
-        <p className="text-gray-600">{selectedTask.description}</p>
-        <p className="text-gray-600 mt-2">Priority: {selectedTask.priority}</p>
-        <p className="text-gray-600 mt-2">Due Date: {selectedTask.dueDate}</p>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 mt-4 rounded"
-          onClick={closeModal}
-        >
-          Close
-        </button>
-      </Modal>
-
-
-      )}
-    </div>
-  );
+            contentLabel="Task Details"
+            overlayClassName="modal-overlay" // Add a custom class name for the overlay
+          >
+            <h2 className="text-lg font-bold mb-4">{selectedTask.title}</h2>
+            <p className="text-gray-600">{selectedTask.description}</p>
+            <p className="text-gray-600 mt-2">
+              Priority: {selectedTask.priority}
+            </p>
+            <p className="text-gray-600 mt-2">
+              Due Date: {selectedTask.dueDate}
+            </p>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 mt-4 rounded"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </Modal>
+        )}
+      </div>
+    );
+  };
 };
-
 export default Calendar;
